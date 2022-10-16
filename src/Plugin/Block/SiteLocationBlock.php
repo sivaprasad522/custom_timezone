9b04b3b61e3c1a98a84a0e6b943ee44a7e9131dd
Sivaprasad <sivaprasad522@gmail.com>
2022-10-17 00:28:53 +0530

    Block plugin and twig template are added

---
+++ src/Plugin/Block/SiteLocationBlock.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\custom_timezone\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a block to display 'Site Location and current time' elements.
+ *
+ * @Block(
+ *   id = "site_location_current_time_block",
+ *   admin_label = @Translation("Site location and current time"),
+ *   category = @Translation("Custom"),
+ * )
+ */
+class SiteLocationBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Stores the configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Stores the siteLocationResolver service.
+   *
+   * @var \Drupal\custom_timezone
+   */
+  protected $siteLocationResolver;
+
+  /**
+   * Stores the list of zones from Utility function.
+   *
+   * @var \Drupal\custom_timezone\Utils
+   */
+  protected $zonesList;
+
+  /**
+   * Creates a SiteLocationBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param mixed $site_location_resolver
+   *   The plugin siteLocationResolver definition.
+   * @param mixed $zones_list
+   *   The Utility Helper function.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory, $site_location_resolver, $zones_list) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+    $this->siteLocationResolver = $site_location_resolver;
+    $this->zonesList = $zones_list;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+      $container->get('custom_timezone.site_location_resolver'),
+      $container->get('custom_timezone.zones'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $site_location_config = $this->configFactory->get('custom_timezone.location');
+    // $service = \Drupal::service('custom_timezone.site_location_resolver');
+    $zones = $this->zonesList->getZones();
+    $timezone = $site_location_config->get('site_timezone.default');
+    $timezone_continent = explode("/", $timezone);
+    $zone_name = $zones[$timezone_continent[0]][$timezone]->render();
+
+    $build = [
+      '#theme' => 'site_location_block',
+      '#data' => [
+        'country' => $site_location_config->get('country'),
+        'city' => $site_location_config->get('city'),
+        'timezone_name' => $zone_name,
+        'time' => $this->siteLocationResolver->getCurrentDateTime('g:i a'),
+        'date' => $this->siteLocationResolver->getCurrentDateTime('l, j F Y'),
+      ],
+    ];
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    return Cache::mergeTags(
+      parent::getCacheTags(),
+      $this->configFactory->get('custom_timezone.location')->getCacheTags()
+    );
+  }
+
+}
---
+++ src/SiteLocationResolver.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\custom_timezone;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Config\ConfigFactoryInterface;
+
+/**
+ * Event handler that gives date and time based on site timezone configuration.
+ */
+class SiteLocationResolver {
+
+  /**
+   * The config name.
+   *
+   * @var string
+   */
+  protected $configName = 'custom_timezone.location';
+  /**
+   * The config.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * SiteLocationResolver constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+
+  }
+
+  /**
+   * Retrieve a date value containing a date and time.
+   *
+   * @param string $format
+   *   Date format option to format the date object.
+   *
+   * @return string
+   *   The current date and time as per the settings.
+   */
+  public function getCurrentDateTime($format = 'jS M Y - g:i A') {
+    $config = $this->configFactory->get($this->configName);
+
+    $current_time = new DrupalDateTime('now', 'UTC');
+    $current_timezone = $config->get('site_timezone.default');
+
+    $siteTimezone = new \DateTimeZone($current_timezone);
+    $date_time = new DrupalDateTime();
+    $timezone_offset = $siteTimezone->getOffset($date_time->getPhpDateTime());
+
+    $time_interval = \DateInterval::createFromDateString($timezone_offset . ' seconds');
+    $current_time->add($time_interval);
+    return $current_time->format($format);
+
+  }
+
+}
